ea37a1bdf96a09dde81b11ffbd27ec32e2c74011
unknown <leo.doray@gmail.com>
2022-09-19 11:11:34 +0200

    tabs 50%

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,29 +5,82 @@ import SectionEnd from '../components/section-end/Section-end';
 import Projet from '../components/projet/Projet';
 import Tab from "../components/skill/Tab";
 
-const tabContent = [
+const tabContent = 
+[
     {
-        title: "Paris",
-        content: "75"
+        "title" : "Front End",
+        "skills" :
+        [
+            {
+                content: "HTML",
+                logo:"sss"
+            },
+            {
+                content: "CSS",
+                logo:"sss"
+            },
+            {
+                content: "JavaScript",
+                logo:"sss"
+            },
+            {
+                content: "React",
+                logo:"sss"
+            },
+            {
+                content: "Git", 
+                logo:"sss"
+            },
+            {
+                content: "Bootstrap",
+                logo:"sss"
+            },
+            {
+                content: "Handlebars",
+                logo:"sss"
+            },
+            {
+                content: "Sass",
+                logo:"sss"
+            },
+        ]
     },
     {
-        title: "Sarthe",
-        content: "72"
-    },
+        "title" : "Back End",
+        "skills" :
+        [
+            {
+                "content": "Node JS",
+            },
+            {
+                "content": "ExpressJS",
+            },
+            {
+                "content": "MySQL",
+            },
+            {
+                "content": "JavaScript",
+            },
+            {
+                "content": "Git",
+            },
+        ]
+    }
 ]
 
-const Home = () => {    
+
+const Home = () => {
     return (
         <div class="bg-secondary">
-            <Header/>
-            <Presentation/>
-            <SectionEnd/>
-            <Projet/>
-            <SectionEnd/>
+            <Header />
+            <Presentation />
+            <SectionEnd />
+            <Projet />
+            <SectionEnd />
             <Tab>
                 {tabContent.map((tab, idx) => (
                     <Tab.TabPane key={`Tab-${idx}`} tab={tab.title}>
-                        {tab.content}
+                        {tab.skills.content}
                     </Tab.TabPane>
                 ))}
             </Tab>
