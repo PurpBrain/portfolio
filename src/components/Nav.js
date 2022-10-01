1d7b825edd29ada9333489faeafa472ef8a5cdab
unknown <leo.doray@gmail.com>
2022-10-01 16:23:05 +0200

    hamburger 50%

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,79 @@
 import React from 'react';
+import { Disclosure } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const navigation = [
+  { name: 'Projets', href: '#projet', current: false },
+  { name: 'Langages', href: '#', current: false },
+  { name: 'À-Propos', href: '#', current: false },
+  { name: 'Contact', href: '#', current: false },
+]
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 function Navs() {
   return (
-    <div className="xl:px-[250px] lg:px-[100px] md:px-[50px] z-50 fixed w-full border-b py-[15px] text-base font-Helvetica text-headline bg-secondary flex justify-between">
-      <p>Léo Doray</p>
-      <div className="flex ">
-        <ul className='flex flex-row'>
-          <li className="mr-[40px]">
-            <a href='/'>Projets</a>
-          </li>
-          <li className="mr-[40px]">
-            <a href='/'>Langages</a>
-          </li>
-          <li className="mr-[40px]">
-            <a href='/' >À-Propos</a>
-          </li>
-          <li>
-            <a href='/' >Contact</a>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <Disclosure as="nav" className="bg-secondary">
+      {({ open }) => (
+        <>
+          <div className="border-b bg-secondary w-full z-50 fixed xl:px-[250px] lg:px-[100px] md:px-[50px] px-[20px] text-base font-Helvetica text-headline">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="sm:hidden sm:w-0">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="flex rounded-md text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <p className='hidden sm:block flex items-center justify-end'>Léo Doray</p>
+              <div className="flex">
+                <div className="hidden sm:block">
+                  <div className="flex space-x-10">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          ' py-2 rounded-md text-sm font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden pt-[65px] z-50 text-base font-Helvetica text-headline">
+            <div className="space-y-1 px-2 pt-2 pb-3">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block px-3 py-2 rounded-md text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   );
 };
 
